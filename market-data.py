ea7e0f45e59915bf1d1fc92c087c5f73adbf547a
KC <kennychan@dbpower.com.hk>
2020-12-14 10:24:30 +0800

    Updated market-data.py

--- market-data.py
+++ market-data.py
@@ -25,22 +25,26 @@ def get_date(df2, cur_second2):
                'open': res2['open'], 'close': res2['close'], 'vol': res2['volume']}
         # df2 = df2.append(row, ignore_index=True)
         # 2.0
-        if (udate2.weekday() in [1, 2, 3, 4] and (udate2.hour <= 14 or udate2.hour >= 19)) \
-                or (udate2.weekday() in [5] and udate2.hour <= 16) \
-                or (udate2.weekday() in [7] and udate2.hour >= 17):
+        if (udate2.weekday() in [0, 1, 2, 3] and (udate2.hour <= 14 or udate2.hour >= 17)) \
+                or (udate2.weekday() in [4] and udate2.hour <= 16) \
+                or (udate2.weekday() in [6] and udate2.hour >= 17):
             # 3.0
             db = sqlite3.connect(path_db)
             cursor = db.cursor()
             cursor.execute('REPLACE INTO nq (udate, high, low, open, close, vol) VALUES (?, ?, ?, ?, ?, ?)', [v for k, v in row.items()])
             if cursor.rowcount == 1:
-                print(udate2, udate2.weekday())
+                print('Insert Success:{}  weekday:{}'.format(udate2, udate2.weekday()))
+            else:
+                print('Insert Fail:{}  weekday:{}'.format(udate2, udate2.weekday()))
             db.commit()
             cursor.close()
             db.close()
             return df2
         else:
+            print('Not Trading Time:{}  weekday:{}'.format(udate2, udate2.weekday()))
             return df2
     else:
+        print('No Data:{}  weekday:{}'.format(datetime.today().strftime("%Y-%m-%d %H:%M:%S"), datetime.today().weekday()))
         return df2
 
 
@@ -51,5 +55,5 @@ while True:
         if cur_second % 5 == 0:
             df1 = get_date(df1, cur_second)
         time.sleep(1)
-    finally:
-        pass
+    except:
+        print('Except: {}'.format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
